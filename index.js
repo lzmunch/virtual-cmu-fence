74b0aa3064a99a3055a25ae905a9a40fc916cdb5
lzhang17 <lzmunch@gmail.com>
2019-11-04 18:13:35 -0500

    added icons, upload file functionality, split html into index and import to do this

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 var express = require('express'),
     path = require('path'),
+    formidable = require('formidable'),
     app = express();
 
 app.set('port', (process.env.PORT || 8080));
 
 app.use(express.static('public'));
+
+// assume called from import.html
+app.get('/', function (req, res){
+    res.sendFile(__dirname + '/public/import.html');
+});
+
+// assume called from import.html
+app.post('/', function (req, res){
+    var form = new formidable.IncomingForm();
+
+    form.parse(req);
+
+    form.on('fileBegin', function (name, file){
+//        file.path = __dirname + '/uploads/' + file.name; 
+        file.path = __dirname + '/public/assets/uploads/' + "tex_image.jpg"; 
+    });
+
+    form.on('file', function (name, file){
+        console.log('Uploaded ' + file.name);
+    });
+
+    res.sendFile(__dirname + '/public/import.html');
+});
 
 app.listen(app.get('port'), function(err) {
   if (err) {
--- public/scripts.js
+++ public/scripts.js
@@ -1,73 +1,83 @@
+var mtlTexFileStr = "fence_model_v1.jpg";
+var mtlFileStr = 
+  "newmtl Solid\n"+
+  "Ka 1.0 1.0 1.0\n"+
+  "Kd 1.0 1.0 1.0\n"+
+  "Ks 0.0 0.0 0.0\n"+
+  "d 1.0\n"+
+  "Ns 0.0\n"+
+  "illum 0\n"+
+  "\n"+
+  "newmtl fence_model_v1\n"+
+  "Ka 1.0 1.0 1.0\n"+
+  "Kd 1.0 1.0 1.0\n"+
+  "Ks 0.0 0.0 0.0\n"+
+  "d 1.0\n"+
+  "Ns 0.0\n"+
+  "illum 0\n"+
+  "map_Kd " + mtlTexFileStr + "\n";
+
+console.log(mtlFileStr);
+
+// THREE.js setup
 var scene = new THREE.Scene();
 
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 camera.position.z = 100;
 
 var renderer = new THREE.WebGLRenderer();
-//renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setSize( 400, 400 );
-//document.body.appendChild( renderer.domElement );
+
+// on document load
 $( function() {
   document.getElementById("import").appendChild( renderer.domElement );
+  loadObjMtl('/assets/models/r2-d2/', 
+           'r2-d2.obj', 'r2-d2.mtl');
 });
 
-
+// controls
 var controls = new THREE.OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 controls.dampingFactor = 0.25;
 controls.enableZoom = true;
 
+// lighting
 var keyLight = new THREE.DirectionalLight(new THREE.Color('hsl(30, 100%, 75%)'), 1.0);
 keyLight.position.set(-100, 0, 100);
-
 var fillLight = new THREE.DirectionalLight(new THREE.Color('hsl(240, 100%, 75%)'), 0.75);
 fillLight.position.set(100, 0, 100);
-
 var backLight = new THREE.DirectionalLight(0xffffff, 1.0);
 backLight.position.set(100, 0, -100).normalize();
-
 scene.add(keyLight);
 scene.add(fillLight);
 scene.add(backLight);
 
 var origObject, scribbleObject; //can delete original and new objects
 
-function loadObjMtl(path, objFile, mtlFile, pos){
+// object loading with texture
+function loadObjMtl(path, objFile, mtlFile, pos, paths){
   var mtlLoader = new THREE.MTLLoader();
-  mtlLoader.setTexturePath(path);
-  mtlLoader.setPath(path);
+  mtlLoader.setTexturePath(paths ? paths.tex : path);
+  mtlLoader.setPath(paths ? paths.mtl : path);
   mtlLoader.load(mtlFile, function (materials) {
 
       materials.preload();
 
       var objLoader = new THREE.OBJLoader();
       objLoader.setMaterials(materials);
-      objLoader.setPath(path);
+      objLoader.setPath(paths? paths.obj : path);
       objLoader.load(objFile, function (object) {
 
           scene.add(object);
-          object.position.y -= 60;
-          object.position.z -= 50;
+          object.position.y += 10;
+          object.position.z += 50;
           console.log("loaded", object.position);
       });
 
   });  
 }
 
-//loadObjMtl('/examples/3d-obj-loader/assets/', 
-loadObjMtl('/assets/models/r2-d2/', 
-           'r2-d2.obj', 'r2-d2.mtl');
-
-//document.addEventListener('keyup', (e) => {
-//  if (e.code === "ArrowUp")
-//    loadObjMtl('/examples/3d-obj-loader/assets/', 
-//               'r2-d2.obj', 'r2-d2_scribble.mtl');
-//  else if (e.code === "ArrowDown")
-//    loadObjMtl('/examples/3d-obj-loader/assets/', 
-//               'r2-d2.obj', 'r2-d2_scribble.mtl');
-//  
-//})
-
+// change tex functions
 function changeToScribble(){
   loadObjMtl('/assets/models/r2-d2/', 
                'r2-d2.obj', 'r2-d2_scribble.mtl');
@@ -80,7 +90,17 @@ function changeToOriginal(){
 
 function changeToFence(){
   loadObjMtl('/assets/models/fence/', 
-               'fence_model_v1.obj', 'fence_model_v1.mtl', );
+               'fence_model_v1.obj', 'fence_model_v1.mtl');
+}
+
+// https://i.imgur.com/3lo7B4V.jpg
+// change texture of fence to image url in input field
+function changeTex(){
+  var url = document.getElementById("img-url-input").value;
+  console.log(url);
+  var mtlFile = mtlTexFileStr;
+  loadObjMtl('/assets/models/fence/', 
+             'fence_model_v1.obj', 'fence_model_v1_scribble.mtl');
 }
 
 //TODO
@@ -89,6 +109,14 @@ function removeEntity(object) {
     scene.remove( selectedObject );
     animate();
 }
+
+function showUploadedImage(){
+    console.log("show image");
+    var img = document.createElement("img");
+    img.src = "/assets/uploads/tex_image.jpg";
+    document.getElementById("uploaded-img").appendChild(img);
+}
+
 
 var animate = function () {
 	requestAnimationFrame( animate );
